0ad6b3693b6c578cb4be0d12c2c2fba24a038c98
Kolawat Inpan <dominolity@gmail.com>
2023-09-26 10:35:08 +0700

    change

--- recursive/recursive1.py
+++
@@ -1,7 +0,0 @@
-def factorial(n):
-    if n == 1 or n == 0:
-        return 1
-    else:
-        return n * factorial(n-1)
-num = int(input("Enter Number : "))
-print(f'{num}! = {factorial(num)}')
--- test2.py
+++ test2.py
@@ -1,7 +1,16 @@
-def fibo(n):
-    if n <= 1:
-        return (n, 0)
-    else:
-        (a, b) = fibo(n-1)
-        return (a+b, a)
-print(fibo(10)[0])
+class Node:
+    order = 0
+    def __init__(self, data):
+        self.data = [Node.order, data]
+        self.left = None
+        self.right = None
+        Node.order += 1
+    
+    def __str__(self):
+        return str(self.data)
+
+Node1 = Node(5)
+Node2 = Node(4)
+Node3 = Node(4)
+print(Node3)
+
